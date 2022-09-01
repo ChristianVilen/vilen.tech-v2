0673bc7a0b7cc1f610b2cc9deff30582ddd4a930
Christian Vilen <christian.vilen@siili.com>
2022-09-01 18:48:20 +0300

    init

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,23 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github"
+import { env } from "../../../env/server.mjs";
+
+export const authOptions: NextAuthOptions = {
+  // Include user.id on session
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  providers: [
+    GithubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
